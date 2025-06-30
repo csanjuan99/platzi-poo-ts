53ff4af96be2f6fa673cbc924c99fa7033d232b6
Carlos Sanjuan <hello@carlossanjuan.co>
2025-06-30 11:00:36 -0500

    2. Methods

---
+++ Auto.ts
@@ -0,0 +1,36 @@
+export class Auto {
+    brand: string;
+    model: string;
+    year: number;
+
+    constructor(
+        brand: string = "Default Brand",
+        model: string = "Default Model",
+        year: number = 2020
+    ) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    print(): string {
+        return `${this.brand} ${this.model} (${this.year})`;
+    }
+
+    setBrand(brand: string): void {
+        this.brand = brand;
+    }
+}
+
+// const auto = new Auto();
+const custom = new Auto("Toyota", "Corolla", 2021);
+
+
+// console.log('Defecto: ', auto);
+// console.log('Personalizado: ', custom);
+
+console.log(custom.print()); // -> Toyota Corolla (2021)
+
+custom.setBrand("Honda");
+
+console.log(custom.print()); // -> Honda Corolla (2021)
